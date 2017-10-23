68ece58987c9f55ff7e9960ac2115e28f3228c47
Wusch <florian.otto@mindbird.de>
2017-10-23 14:03:16 +0200

    Initial

--- src/ProductLinkBundle.php
+++ src/ProductLinkBundle.php
@@ -2,8 +2,19 @@
 
 namespace Mindbird\Isotope\ProductLink;
 
+use Contao\ManagerPlugin\Config\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Config\FileLocator;
 
 class ProductLinkBundle extends Bundle
 {
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('services.yml');
+    }
 }
